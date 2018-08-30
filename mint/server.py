afd8ec05cc60cdeba1be1d388674c6b72573de01
mugbya <mugbya@gmail.com>
2018-08-30 17:14:57 +0800

    代码优化

--- mint/server.py
+++ mint/server.py
@@ -2,13 +2,10 @@ from sanic import Sanic
 from sanic.response import json
 from mint import settings
 import requests
-from mint.common import redis_client
-from mint.util.sanic_jinja import render
+
 from lxml import html
 
 app = Sanic()
-app.static('/static', settings.STATIC_URL)
-app.static('/media', settings.MEDIA_URL)
 
 
 @app.route('/')
@@ -31,22 +28,8 @@ async def share_uri(request):
         return json({'code': -1, 'message': '视频链接提取失败'})
 
     video_url = video_url.replace('playwm', 'play')
-    # 将视频链接存到redis后续在处理,
-    # redis_client.lpush("%s:start_urls" % settings.SPIDER_NAME, video_url)
 
-    # 返回一个能读取视频的链接
-    # new_url = request.host + '/play/?' + video_url
     return json({'code': 0, 'message': None, 'rsp': video_url})
-
-
-@app.get('/play')
-async def play(request):
-    key = request.query_string
-
-    sign = redis_client.hget('mint', key)
-    sign = str(sign, encoding="utf-8")
-    return render('index.html', request, sign=sign)
-
 
 if __name__ == '__main__':
     app.run(host=settings.HOST, port=settings.PORT)
--- mint/settings.py
+++ mint/settings.py
@@ -2,21 +2,8 @@ import os
 
 PACKAGE_NAME = os.path.basename(os.getcwd())
 
-
-STATIC_URL = 'webContent/static'
-MEDIA_URL = '/root/PTokTikTask/crawler'
-# MEDIA_URL = '/Users/mugbya'
-TEMPLATES = "webContent/templates"
-
-REDIS_HOST = '127.0.0.1'
-REDIS_PORT = '6379'
-REDIS_DB = '0'
-REDIS_PASSWORD = ''
-
-SPIDER_NAME = 'toktik'
-
 HOST = '0.0.0.0'
-PORT = 8000
+PORT = 80
 
 
 
